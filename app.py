3c835c8944dc1abe4e7e0b99078689d56cd2d5c5
lijian <lijiancode@gmail.com>
2019-04-19 18:29:35 +0800

    add remote video and vtt

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask import render_template
 import flaskr.db as db
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route('/')
@@ -15,7 +17,7 @@ def hello():
 def show(cartoon_id):
     cartoon = db.get_cartoon(cartoon_id)
     subtitle = str(cartoon[2]).split(".")
-    return render_template("show.html", cartoon=cartoon, subtitle=subtitle)
+    return render_template("show.html", cartoon=cartoon, subtitle=subtitle, file_id=cartoon_id)
 
 
 if __name__ == '__main__':
